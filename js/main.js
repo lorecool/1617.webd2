c6306dbb19da1ef6763931847b6286ba529c40c3
lorecool <lorenzo.coole@student.arteveldehs.be>
2017-05-24 18:23:08 +0200

    Version 25/05

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-;(function() {
-  alert('Welcome to the GDM Web Template developed by drdynscript!');
-})();
+// When the user clicks on div, open the popup
+function portfolioDetail() {
+    var popup = document.getElementById("portfolioDetail");
+    popup.classList.toggle("show");
+}
+
+function disclaimerDetail() {
+    var popup = document.getElementById("disclaimerDetail");
+    popup.classList.toggle("show");
+}
+
+function copyrightYear() {
+    var currentYear = new Date().getFullYear();
+    document.write(currentYear);
+}

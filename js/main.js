fcc5a18a16e1bc9cec8242bef22ddad64ae70935
lorecool <lorenzo.coole@student.arteveldehs.be>
2017-05-25 10:59:05 +0200

    update menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 // When the user clicks on div, open the popup
+function openNav() {
+    document.getElementById("sideNav").style.width = "100%";
+    document.getElementById("menu").style.height = "0px";
+
+}
+
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+    document.getElementById("menu").style.height = "1000px";
+}
+
 function portfolioDetail() {
     var popup = document.getElementById("portfolioDetail");
     popup.classList.toggle("show");

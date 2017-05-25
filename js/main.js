48cd56f03217fcefb92e02058db1ba9b9283cbb9
lorecool <lorenzo.coole@student.arteveldehs.be>
2017-05-25 11:18:31 +0200

    scroll button

--- js/main.js
+++ js/main.js
@@ -10,6 +10,28 @@ function closeNav() {
     document.getElementById("menu").style.height = "1000px";
 }
 
+//Scroll knop
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
+        document.getElementById("naarBoven").style.display = "block";
+    } else {
+        document.getElementById("naarBoven").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+
+
+
 function portfolioDetail() {
     var popup = document.getElementById("portfolioDetail");
     popup.classList.toggle("show");
